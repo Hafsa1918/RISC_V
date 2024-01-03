e8133e091f2fd8ad0fe06cda77da2cf7ff6d0ec6
ha007-aman <73087518+ha007-aman@users.noreply.github.com>
2024-01-03 21:15:07 +0500

    Update tb_risc.v

--- TestBench/tb_risc.v
+++ TestBench/tb_risc.v
@@ -1,1 +1,16 @@
-frf
+module tb_risc();
+reg clk,reset;
+  
+risc_v dut(clk,reset);
+  
+always #5 clk=~clk;
+  
+initial 
+begin
+ clk =0; reset = 1;
+ #10;
+ reset = 0;
+ #200 $stop;
+end
+  
+endmodule 
